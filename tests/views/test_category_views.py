336fbd30da0c3f329196cd1c291f762d5fd36ef3
Jeffrey Hover <jshover2000@gmail.com>
2026-06-09 17:10:49 -0700

    test(Test Creation): :construction: Wrote some tests for category views but WIP

--- tests/views/test_category_views.py
+++ tests/views/test_category_views.py
@@ -2,6 +2,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
 from django.contrib.auth.models import User
+from django.urls import reverse
 from finances.category.models import Category
 
 
@@ -32,7 +33,7 @@ class TestCategoryViews:
             color="#FF0000",
         )
 
-        response = self.client.get("/api/categories/")
+        response = self.client.get(reverse("category-list"))
         assert response.status_code == status.HTTP_200_OK
         assert isinstance(response.data, list)
         assert len(response.data) == 1
@@ -40,13 +41,13 @@ class TestCategoryViews:
 
     def test_successful_create_category(self):
         data = {"name": "Testing", "category_type": "expense", "color": "#FF5733"}
-        response = self.client.post("/api/categories/", data=data)
+        response = self.client.post(reverse("category-list"), data=data)
         assert response.status_code == status.HTTP_201_CREATED
         assert Category.objects.filter(name="Testing", user=self.user).exists()
 
     def test_failed_create_category_with_invalid_data(self):
         data = {"name": "", "category_type": "invalid_type", "color": "not_a_color"}
-        response = self.client.post("/api/categories/", data=data)
+        response = self.client.post(reverse("category-list"), data=data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert not Category.objects.filter(name="", user=self.user).exists()
 
@@ -60,7 +61,7 @@ class TestCategoryViews:
             "color": "#00FF00",
             "user": another_user.id,
         }
-        response = self.client.post("/api/categories/", data=data)
+        response = self.client.post(reverse("category-list"), data=data)
         assert response.status_code == status.HTTP_201_CREATED
         category = Category.objects.get(name="Testing")
         assert category.user == self.user
@@ -68,5 +69,5 @@ class TestCategoryViews:
     def test_unauthenticated_user_cannot_create_category(self):
         self.client.force_authenticate(user=None)
         data = {"name": "Testing", "category_type": "expense", "color": "#FF5733"}
-        response = self.client.post("/api/categories/", data=data)
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        response = self.client.post(reverse("category-list"), data=data)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
